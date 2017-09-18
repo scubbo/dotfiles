583eff5b9db0f02863a8cb0822224ca70897892b
Scubbo <noreply@github.com>
2017-09-18 10:59:49 -0700

    Remove docker and NVM

--- setup.sh
+++ setup.sh
@@ -37,3 +37,5 @@ cd /tmp
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip
 sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+
+echo "Now go to https://w.amazon.com/index.php/BrazilCLI_2.0/GettingStarted to install Brazil"
