86f460c7a06505a0546ee0ae6b96766ebae11350
Jack Jackson <scubbojj@gmail.com>
2022-04-03 22:45:19 -0700

    Add more indexed functions

--- bin/indexed-funcs.zsh
+++ bin/indexed-funcs.zsh
@@ -1,5 +1,5 @@
 # https://stackoverflow.com/a/49105399/1040915
-declare -a funcs=(psearch tabname ff)
+declare -a funcs=(psearch tabname ff grhh grup)
 # AFAIK, there's no way to ensure that these "associative arrays"
 # (Dictionaries, basically) have key-sets that match `funcs` -
 # nor have I figured out how to do the obvious thing of
@@ -9,12 +9,16 @@ declare -A docs=(
     [psearch]="Search ps"
     [tabname]="Change the name of the current tab"
     [ff]="Follow File (tail with an empty screen)"
+    [grhh]="Git Reset Hard Head"
+    [grup]="Git Reset to UPstream"
 )
 
 declare -A defs=(
     [psearch]="ps aux | grep -i $1 | grep -v 'grep'"
     [tabname]='printf "\e]1;$1\a"'
     [ff]="clear; tail -Fn0"
+    [grhh]="git reset --hard HEAD"
+    [grup]="git reset --hard @{u}"
 )
 
 # https://unix.stackexchange.com/a/287333/30828
