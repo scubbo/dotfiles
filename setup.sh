1396621f4ccf3962595272cf299c1f588a3a10d8
Jack Jackson <scubbojj@gmail.com>
2022-09-05 03:17:24 -0700

    Sublime keymapping

--- setup.sh
+++ setup.sh
@@ -3,7 +3,6 @@
 set -e
 
 # Install dotfiles
-rm -f $HOME/.gitconfig # It might have got added while you were doing other things, and ln would complain
 ln -s ~/Code/dotfiles/zshrc $HOME/.zshrc
 arch_name=$(uname -m)
 if [[ "$arch_name" == "x86_64" ]]; then
@@ -16,11 +15,26 @@ else
   echo "Unrecognized architecture: $arch_name"
   exit 1
 fi
+
 ln -s ~/Code/dotfiles/gitignore_global $HOME/.gitignore_global
+rm -f $HOME/.gitconfig # It might have got added while you were doing other things, and ln would complain
 ln -s ~/Code/dotfiles/gitconfig $HOME/.gitconfig
 ln -s ~/Code/dotfiles/vimrc $HOME/.vimrc
 ln -s ~/Code/dotfiles/envFolder $HOME/.env
 ln -s ~/Code/dotfiles/bin $HOME/bin
 ln -s ~/Code/dotfiles/screenrc $HOME/.screenrc
+
+# Install Sublime keybindings, if sublime is present
+SUBLIME_KEYMAPPING_DIR="$HOME/Library/Application Support/Sublime Text"
+SUBLIME_KEYMAPPING_FILE="$SUBLIME_KEYMAPPING_DIR/Default (OSX).sublime-keymap"
+if [ -d "$SUBLIME_KEYMAPPING_DIR" ]; then
+  if [ -e "$SUBLIME_KEYMAPPING_FILE" ]; then
+    echo "User key-mapping file for Sublime already exists, not overwriting"
+  else
+    ln -s ~/Code/dotfiles/sublime-keymapping.json "$SUBLIME_KEYMAPPING_DIR/Default (OSX).sublime-keymap"
+  fi
+else
+  echo "Sublime is not currently installed - Sublime key-mappings not installed"
+fi
 
 echo "Dotfiles installed."
